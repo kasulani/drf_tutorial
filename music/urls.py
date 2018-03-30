8076dad2c26d74018f68da6e67e58a7985ef8512
kasulani <kasulani@gmail.com>
2018-03-30 14:54:28 +0300

    Add url login view
    * add url for login view

--- music/urls.py
+++ music/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ListCreateSongsView, SongsDetailView
+from .views import ListCreateSongsView, SongsDetailView, LoginView
 
 
 urlpatterns = [
     path('songs/', ListCreateSongsView.as_view(), name="songs-list-create"),
-    path('songs/<int:pk>/', SongsDetailView.as_view(), name="songs-detail")
+    path('songs/<int:pk>/', SongsDetailView.as_view(), name="songs-detail"),
+    path('auth/login/', LoginView.as_view(), name="auth-login")
 ]
