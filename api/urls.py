1821d95d56bb11687c9d18579230a45c3e08b6c6
kasulani <kasulani@gmail.com>
2018-03-30 14:48:45 +0300

    Update urls
    * refactor path to re_path for songs endpoints
    * add path to endpoint for obtaining token

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/<version>(v1|v2)/', include('music.urls'))
+    path('api-token-auth/', obtain_jwt_token, name='create-token'),
+    re_path('api/(?P<version>(v1|v2))/', include('music.urls'))
 ]
